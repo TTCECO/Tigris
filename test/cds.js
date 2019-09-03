07eaeed66a770662479cfd0ebaf38e343dbfc586
TATAUFO <liupeng@tataufo.com>
2019-09-03 16:46:03 +0800

    refactor

--- test/cds.js
+++ test/cds.js
@@ -1,24 +1,25 @@
 var CUSD = artifacts.require("./CFIAT.sol");
 var CDS = artifacts.require("./CDS.sol");
 var USDTTC = artifacts.require("./ORACLE.sol");
+var CLAY = artifacts.require("./CLAY.sol");
 
 contract('CDS', function() {
 	var eth = web3.eth;
   var owner = eth.accounts[0];
+  var operator_1 = eth.accounts[1];
 
 
 	it("get admin ",async () =>  {
         const cds = await CDS.deployed();
-        const cusd = await CUSD.deployed();
         admin = await cds.admin.call()
         assert.equal(admin, owner, "equal")
   });
 
   it("admin set operator  ",async () =>  {
         const cds = await CDS.deployed();
-        await cds.addOperator(owner, {from:owner});
+        await cds.addOperator(operator_1, {from:owner});
         operator = await cds.getOperators.call();
-        assert.equal(operator[0], owner, "equal");
+        assert.equal(operator[0], operator_1, "equal");
   });
 
 });
