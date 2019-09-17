7ffcb39008f96a32da68541d6710cdc08494edfe
TATAUFO <liupeng@tataufo.com>
2019-09-17 11:12:31 +0800

    refactor orcale interface

--- contracts/OracleInterface.sol
+++ contracts/OracleInterface.sol
@@ -1,7 +1,5 @@
 pragma solidity ^0.4.19;
 
 interface ORC {
-    function setValidDistance(uint _validDistance)  public;
-	function setValue(uint _value) public ;
-	function getValue(uint _blockNumber) public view returns (uint);
+	function getLatestValue() public view returns (uint);
 }
